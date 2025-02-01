436fa0c88056780ad8756b586a822efdc05c271d
Roman Yaurov <yaurov.r@gmail.com>
2025-02-01 14:32:38 +0300

    [shared/ui/Layout] refactor: define change route animation direction

--- src/shared/ui/Layout/Layout.tsx
+++ src/shared/ui/Layout/Layout.tsx
@@ -7,24 +7,51 @@ type LayoutProps = {
   footer: ReactNode;
 };
 
+enum AnimationDirections {
+  FROM_RIGHT_TO_LEFT = 'fromRightToLeft',
+  FROM_LEFT_TO_RIGHT = 'fromLeftToRight',
+}
+
 export function Layout({ header, footer }: LayoutProps) {
+  const routes = ['/add', '/', '/profile'];
+
   const location = useLocation();
   const newOutlet = useOutlet();
-  
+
   const [currentOutlet, setCurrentOutlet] = useState<ReactNode>(newOutlet);
   const [prevOutlet, setPrevOutlet] = useState<ReactNode | null>(null);
+  const [currentPath, setCurrentPath] = useState<string>(location.pathname);
+  const [prevPath, setPrevPath] = useState<string | null>(null);
   const [isAnimating, setIsAnimating] = useState<boolean>(false);
 
+  const defineAnimationDirection = () => {
+    const prevPathIndex = routes.findIndex((path) => path === prevPath);
+    const currentPathIndex = routes.findIndex((path) => path === currentPath);
+
+    if (prevPathIndex < currentPathIndex) {
+      return AnimationDirections.FROM_RIGHT_TO_LEFT;
+    } else {
+      return AnimationDirections.FROM_LEFT_TO_RIGHT;
+    }
+  }
+
   useEffect(() => {
-    if (newOutlet !== currentOutlet) {
+    if (location.pathname !== currentPath) {
       setPrevOutlet(currentOutlet);
+      setPrevPath(currentPath);
       setCurrentOutlet(newOutlet);
+      setCurrentPath(location.pathname);
       setIsAnimating(true);
 
-      setTimeout(() => {
+      const timeout = setTimeout(() => {
         setPrevOutlet(null);
+        setPrevPath(null);
         setIsAnimating(false);
-      }, 500);
+      }, 300);
+
+      return () => {
+        clearTimeout(timeout);
+      };
     }
   }, [location.pathname]);
 
@@ -32,12 +59,14 @@ export function Layout({ header, footer }: LayoutProps) {
     <div className={styles.root}>
       {header}
       <div className={styles.container}>
-        {prevOutlet && (
-          <div className={`${styles.content} ${styles.slideOut}`}>
+        {prevOutlet && isAnimating && (
+          <div className={`${styles.content} ${styles.slideOut} ${styles[defineAnimationDirection()]}`}>
             {prevOutlet}
           </div>
         )}
-        <div className={`${styles.content} ${isAnimating ? styles.slideIn : ''}`}>
+        <div
+          className={`${styles.content} ${isAnimating ? styles.slideIn : ''} ${isAnimating ? styles[defineAnimationDirection()] : ''}`}
+        >
           {currentOutlet}
         </div>
       </div>
